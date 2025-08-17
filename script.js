27069e4a7c1cafed3e6a306492d58997444d4d6e
mtcho <myintthwaycho11@gmail.com>
2025-08-17 13:40:55 +0700

    add 4 basic math functions +-*/

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(5,2));
+console.log(substract(5,2));
+console.log(multiply(5,2));
+console.log(divide(5,2));
